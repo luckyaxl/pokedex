2cb0f79b7d143f7f974e6a69789df66e4f64cb42
luckyaxl <luckyaxl@gmail.com>
2021-09-24 06:32:33 +0700

    add apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import React from "react";
+import { ApolloProvider } from "@apollo/react-hooks";
+import client from "src/utils/apolloClient";
+
+import "src/styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
