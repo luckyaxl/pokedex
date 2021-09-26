b63b91d8293662a3f587fa07c89e54cf72b3537e
luckyaxl <luckyaxl@gmail.com>
2021-09-26 23:25:52 +0700

    disable PWA on dev env

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require("next-pwa");
+const prod = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
+    disable: prod ? false : true
   },
 });
