fc9ed7fe826e5da4398e856ceae3882314cf524a
luckyaxl <luckyaxl@gmail.com>
2021-09-26 17:49:57 +0700

    add PWA

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
         <body>
           <Main />
           <NextScript />
