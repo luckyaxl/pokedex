3d42dd5dd4cc0d206977d1dfce5ec6d304a4ee35
luckyaxl <luckyaxl@gmail.com>
2021-09-28 06:35:09 +0700

    change app name and info

--- src/components/MetaTags.js
+++ src/components/MetaTags.js
@@ -1,12 +1,12 @@
 import Head from "next/head";
 import React from "react";
 
-function MetaTags({ title = "Pokemon" }) {
+function MetaTags({ title = "Pokester" }) {
   return (
     <Head>
       <title>{title}</title>
-      <meta name="title" content="Pokemon" />
-      <meta name="description" content="Pokomens Character" />
+      <meta name="title" content="Pokester" />
+      <meta name="description" content="Pokemons Character" />
       <meta name="keywords" content="pokemon" />
       <meta name="robots" content="index, follow" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
